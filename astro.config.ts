8fc74a292908e7da763468b37206eab17670602d
mokuzai-cell <7525109@ed.tus.ac.jp>
2025-07-07 18:58:53 +0900

    サンド面正直

--- astro.config.ts
+++ astro.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://mokuzai-cell.github.io', // https://mokuzai-cell.github.io
-    base: 'astro-hands-on', //js-workshopha
+    base: 'js-workshopha', //js-workshopha
 });
